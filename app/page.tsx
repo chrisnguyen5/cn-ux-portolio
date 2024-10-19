44e739bf66b135ef75ecbfb621607847cf1efb70
Chris Nguyen <chrisnguyen5@gmail.com>
2024-10-19 15:27:22 -0700

    test3

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           height={38}
           priority
         /> */}
-        <h1 className="text-3xl font-bold text-red-400 underline">Hello ya fuck!</h1>
+        <h1 className="text-3xl font-bold text-red-400 underline">Hello ya fuckable!</h1>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
