509fc9a26f84a33e72a8d91e6986928c7b29c21a
Chris Nguyen <chrisnguyen5@gmail.com>
2024-10-28 00:19:27 -0700

    content updated

---
+++ components/LinkPreviewDemo.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { LinkPreview } from "@/components/ui/link-preview";
+
+export function LinkPreviewDemo() {
+  return (
+    <div className="flex justify-center items-center h-[35rem] flex-col px-4">
+     
+      <p className="text-neutral-500 dark:text-neutral-400 text-xl md:text-3xl max-w-3xl mx-auto mb-10">
+        Please Visit my{" "}
+        <LinkPreview
+          url="https://chrisnguyen.vercel.app/"
+          className="font-bold bg-clip-text text-transparent bg-gradient-to-br from-purple-500 to-pink-500"
+        >
+          Resume
+        </LinkPreview>{" "}
+        if you would like to learn more about my experiences and expertise.
+      </p>
+      <p className="text-neutral-500 dark:text-neutral-400 text-xl md:text-3xl max-w-3xl mx-auto">
+        My{" "}
+        <LinkPreview url="https://chrisnguyen.vercel.app/contact" className="font-bold">
+          Email
+        </LinkPreview>,{" "} 
+        <LinkPreview url="https://github.com/chrisnguyen5" className="font-bold">
+          GitHub
+        </LinkPreview>
+        ,{" "}and{" "}
+        <LinkPreview url="https://www.linkedin.com/in/chrisnguyen2" className="font-bold">
+          LinkedIn
+        </LinkPreview>{" "}
+        are other great ways to connect as well.
+      </p>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -87,13 +87,24 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  // plugins: [require("tailwindcss-animate")],
+  plugins: [addVariablesForColors],
 } satisfies Config
 
 export default config
 
 
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
 
+  addBase({
+    ":root": newVars,
+  });
+}
 
 
 
