e8fae0bef38048f7d9059e391e3c5139df10bb40
Chris Nguyen <chrisnguyen5@gmail.com>
2024-10-28 18:28:47 -0700

    added cards

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com', 'api.microlink.io'],
+        domains: ['images.unsplash.com', 'api.microlink.io', 'chrisnguyen.x10host.com'],
     },
 };
 
