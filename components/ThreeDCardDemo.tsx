d84bd02760396c1377381cede77f7b817926d99f
Chris Nguyen <chrisnguyen5@gmail.com>
2025-08-19 13:38:43 -0700

    fixed cards to function properly

--- components/ThreeDCardDemo.tsx
+++ components/ThreeDCardDemo.tsx
@@ -44,7 +44,7 @@ export function ThreeDCardDemo() {
          
         </div>
       </CardBody>
-      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+      {/* <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
         <CardItem
           translateZ="50"
           className="text-xl font-bold text-neutral-600 dark:text-white"
@@ -79,8 +79,8 @@ export function ThreeDCardDemo() {
           </CardItem>
           
         </div>
-      </CardBody>
-      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+      </CardBody> */}
+      {/* <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
         <CardItem
           translateZ="50"
           className="text-xl font-bold text-neutral-600 dark:text-white"
@@ -115,7 +115,7 @@ export function ThreeDCardDemo() {
           </CardItem>
           
         </div>
-      </CardBody>
+      </CardBody> */}
     </CardContainer>
     
   );
