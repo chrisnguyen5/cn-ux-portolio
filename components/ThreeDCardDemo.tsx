144488c2cfe19c023d7cd2d89deba646293fc180
Chris Nguyen <chrisnguyen5@gmail.com>
2025-10-01 11:55:44 -0700

    updated sections with content

--- components/ThreeDCardDemo.tsx
+++ components/ThreeDCardDemo.tsx
@@ -39,7 +39,7 @@ export function ThreeDCardDemo() {
             target="__blank"
             className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
           >
-            Learn more →
+            View details
           </CardItem>
          
         </div>
--- components/ThreeDCardDemo2.tsx
+++ components/ThreeDCardDemo2.tsx
@@ -40,7 +40,7 @@ export function ThreeDCardDemo2() {
             target="__blank"
             className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
           >
-            Learn more →
+            View details
           </CardItem>
           
         </div>
