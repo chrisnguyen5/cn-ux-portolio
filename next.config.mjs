fba55b65c755c9e973acc5f1517b990b1b76d3b7
Chris Nguyen <chrisnguyen5@gmail.com>
2025-10-25 14:02:32 -0700

    updated img src folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com', 'api.microlink.io', 'chrisnguyen.x10host.com', 'public.boxcloud.com', 'app.box.com', 'x15.x10hosting.com'],
+        domains: ['images.unsplash.com', 'api.microlink.io', 'chrisnguyen.x10host.com'],
     },
 };
 
